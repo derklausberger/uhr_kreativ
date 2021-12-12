7581dcb67d911cf60d054fb500ee1c40e7874457
derklausberger <niklasklausberger@gmail.com>
2021-12-12 19:33:04 +0100

    changed playGame, checkMomentum, moveBall; add demo youLost;

--- src/main/java/com/example/breakout/Classes/Game.java
+++ src/main/java/com/example/breakout/Classes/Game.java
@@ -11,17 +11,21 @@ public class Game {
     private Ball ball;
     private Bar bar;
     private List<Block> blocks;
+    private Scene scene;
+    private boolean stillPlaying = true;
 
     public Game() {
         ball = null;
         bar = null;
         blocks = null;
+        scene = null;
     }
 
     public Game(Game game) {
         ball = game.ball;
         bar = game.bar;
         blocks = game.blocks;
+        scene = null;
     }
 
     public Game(String filepath) {
@@ -40,43 +44,65 @@ public class Game {
         return null;
     }
 
-    public void moveBall(double x, double y) {
-        ball.moveTo(x, y);
+    public void moveBall() {
+        List<Double> momentum = ball.getmomentum();
+        List<Double> position = ball.getpositionalinfo();
+        checkMomentum();
+        ball.moveTo(position.get(0) + momentum.get(0),
+                position.get(1) + momentum.get(1));
     }
 
     public void moveBar(double x) {
         bar.moveTo(x);
     }
 
-    public void runBar(Scene scene) {
+    public void checkMomentum() {
+        List<Double> position = ball.getpositionalinfo();
+        List<Double> momentum = ball.getmomentum();
 
-    }
+        for (Block block : blocks) {
+            int touches = block.checkblock(position);
+            // ball touches block on left or right side
+            if (touches == 1 || touches == 2) {
+                ball.changemomentum((momentum.get(0) * -1), momentum.get(1));
+            } else if (touches == 3 || touches == 4) { // ball touches block on top or bottom side
+                ball.changemomentum(momentum.get(0), (momentum.get(1) * -1));
+            }
+        }
 
-    public void runBall(Scene scene) {
-        while (true) {
-            scene.setOnKeyPressed(event -> {
-                if (event.getCode() == KeyCode.SPACE) {
-                    ;
-                }
-            });
+        // ball touches left or right side of the window
+        if (position.get(0) - position.get(2) == 0) {
+            ball.changemomentum((momentum.get(0) * -1), momentum.get(1));
+        } else if (position.get(0) + position.get(2) == scene.getWidth()) {
+
+        }
+
+        // ball touches top or bottom side of the window
+        if (position.get(1) - position.get(2) == 0) {
+            ball.changemomentum(momentum.get(0), (momentum.get(1) * -1));
+        } else if (position.get(1) + position.get(2) == scene.getHeight()) {
+            youLost();
         }
     }
 
     public void playGame(String filepath, Scene scene) {
+        this.scene = scene;
         Thread barFred = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    scene.setOnKeyPressed(event -> {
-                        if (event.getCode() == KeyCode.RIGHT) {
-                            moveBar(bar.getX() + 1); //  1 is dummy value
-                            // moveBall() ???
-                        } // else if?? if right and left arrow get pressed
-                          // simultaneously
-                        if (event.getCode() == KeyCode.LEFT) {
-                            moveBar(bar.getX() - 1); //  1 is dummy value
-                        }
-                    });
+                scene.setOnKeyPressed(event -> {
+                    if (event.getCode() == KeyCode.RIGHT) {
+                        moveBar(bar.getX() + 1); //  1 is dummy value
+                        // moveBall() ???
+                    } // else if?? if right and left arrow get pressed
+                    // simultaneously
+                    if (event.getCode() == KeyCode.LEFT) {
+                        moveBar(bar.getX() - 1); //  1 is dummy value
+                    }
+                });
+
+                while (run) {
+
                 }
             }
         });
@@ -85,6 +111,17 @@ public class Game {
             @Override
             public void run() {
                 scene.setOnKeyPressed(event -> {
+                    if (event.getCode() == KeyCode.RIGHT) {
+                        moveBar(bar.getX() + 1); //  1 is dummy value
+                        // moveBall() ???
+                    } // else if?? if right and left arrow get pressed
+                    // simultaneously
+                    if (event.getCode() == KeyCode.LEFT) {
+                        moveBar(bar.getX() - 1); //  1 is dummy value
+                    }
+                });
+
+                scene.setOnKeyPressed(event -> {
                     if (event.getCode() == KeyCode.SPACE) {
                         start();
                     }
@@ -92,17 +129,19 @@ public class Game {
             }
 
             public void start() {
-                while (true) {
-                    List<Double> momentum = ball.getmomentum();
-                    List<Double> position = ball.getpositionalinfo();
-                    moveBall(position.get(0) + momentum.get(0),
-                            position.get(1) + momentum.get(1));
-
+                while (stillPlaying) {
+                    checkMomentum();
+                    moveBall();
                 }
             }
         });
 
-        barFred.start();
+        //barFred.start();
         ballFred.start();
+    }
+
+    public void youLost() {
+        stillPlaying = false;
+        System.out.println("you lost boy");
     }
 }
