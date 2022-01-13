8a52f3f463ca495550d4b869fee609ad5d7c0833
derklausberger <niklasklausberger@gmail.com>
2022-01-13 18:38:51 +0100

    did some changes

--- src/main/java/com/example/breakout/Classes/Block.java
+++ src/main/java/com/example/breakout/Classes/Block.java
@@ -112,7 +112,7 @@ public class Block implements Serializable {
             //if (Math.abs(xdif) > Math.abs(ydif)) {
             if (Math.max(x, ballinfo.get(0)) - Math.min(x, ballinfo.get(0))
                     <= Math.max(y, ballinfo.get(1)) - Math.min(y, ballinfo.get(1))
-                    && xdif > ydif) {
+                    ) {
                 if (xdif > 0) {
                     System.out.println("left");
                     return 1;
